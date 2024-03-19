ad8526a3499cd7b99e0e6004d4752d2c2eceeda6
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-03-19 13:20:34 +0100

    Udpt README.md

--- IHSetExamples/__init__.py
+++ IHSetExamples/__init__.py
@@ -1,5 +1,4 @@
 # src/__init__.py
 
 # Import modules and functions from your package here
-from .jaramillo20 import jaramillo20
-from .calibration import cal_Jaramillo20
+from plot_par_evolution import plot_par_evolution
